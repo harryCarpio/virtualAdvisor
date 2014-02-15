07fe607706aa833d939128e6ab2e6115d133c88b
Harry Carpio <harry.carpio@gmail.com>
2014-02-15 14:59:09 -0500

    Con las variables de Info General y Creditos aprobados, totales y faltantes php ...

--- virtualAdvisor.php
+++ virtualAdvisor.php
@@ -10,8 +10,9 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       $matricula = $_POST["matriculaIn"];
-//
+        $wsdl_url = 'https://ws.espol.edu.ec/saac/wsSAAC.asmx?wsdl';
+        $matricula = $_POST["matriculaIn"];
+
 //        try {
 //            $wsdl_url = 'https://ws.espol.edu.ec/saac/wsSAAC.asmx?wsdl';
 //            $client = new SOAPClient($wsdl_url);
@@ -19,7 +20,7 @@ and open the template in the editor.
 //                'matricula' => "$matricula"
 //            );
 //            $return = $client->MateriasMallaAprobadas($params);
-//            
+//            var_dump($return->MateriasMallaAprobadasResult->any);
 //            $xml = simplexml_load_string($return->MateriasMallaAprobadasResult->any);
 //            $aprobadasArray=$xml->NewDataSet->V_MATERIAS_ACREDITADAS;
 //            $numeroAprobadas = $aprobadasArray->count();
@@ -29,9 +30,11 @@ and open the template in the editor.
 //            echo "Exception occured: " . $e;
 //        }
        
+       /*******************************************************************
+        *      OBTENER INFORMACION GENERAL DESDE LA MATRICULA INGRESADA   *
+        *******************************************************************/
        
-       try {
-            $wsdl_url = 'https://ws.espol.edu.ec/saac/wsSAAC.asmx?wsdl';
+       try {            
             $client = new SOAPClient($wsdl_url);
             $params = array(
                 'matricula' => "$matricula"
@@ -39,7 +42,7 @@ and open the template in the editor.
             $return = $client->InformacionAcademicaEstudianteGet($params);
             
             $xml = simplexml_load_string($return->InformacionAcademicaEstudianteGetResult->any);
-            var_dump($xml->NewDataSet->Table);
+//            var_dump($xml->NewDataSet->Table);
             $ci_student = $xml->NewDataSet->Table->COD_ESTUDIANTE;
             $estado_student = $xml->NewDataSet->Table->ESTADO_ESTUDIANTE;
             $nombres_student = $xml->NewDataSet->Table->NOMBRES;
@@ -50,7 +53,95 @@ and open the template in the editor.
             $nombre_carrera = $xml->NewDataSet->Table->NOMBRE_CARRERA;
             $num_aprobadas = $xml->NewDataSet->Table->NUMAPROBADAS;
             $cod_unidad = $xml->NewDataSet->Table->COD_UNIDAD;
+        
+//           $aprobadasArray=$xml->NewDataSet->V_MATERIAS_ACREDITADAS;
+//            $numeroAprobadas = $aprobadasArray->count();
+//            echo "Materias Aprobadas: ".$numeroAprobadas;
             
+        } catch (Exception $e) {
+            echo "Exception occured: " . $e;
+        }
+        
+        
+        
+        try {
+            $client = new SOAPClient($wsdl_url);
+            $params = array(
+                'codigoEstudiante' => "$matricula",
+                'codigoDivision' => "$cod_division",
+                'codigoCarrera' => "$cod_carrera",
+                'codigoEspecializacion' => "$cod_especializ"                
+            );
+            $return = $client->CreditosEstudiante($params);
+            $resumenCreditosArray = $return->CreditosEstudianteResult->Resumen->Credito;
+            //echo count($resumenCreditosArray);  
+//            var_dump($resumenCreditosArray);
+            foreach ( $resumenCreditosArray as $credito){
+                if($credito->TipoCredito == "OPTATIVA"){
+                    $creditoOptativo = array(
+                        'Tomados' => $credito->Tomados,
+                        'Flujo' => $credito->Flujo,
+                        'Excedente' => $credito->Excedente,
+                        'IdTipoCredito' => $credito->IdTipoCredito
+                    );
+                }
+                if($credito->TipoCredito == "LIBRE OPCION"){
+                    $creditoLibreOpcion = array(
+                        'Tomados' => $credito->Tomados,
+                        'Flujo' => $credito->Flujo,
+                        'Excedente' => $credito->Excedente,
+                        'IdTipoCredito' => $credito->IdTipoCredito
+                    );
+                }
+                if($credito->TipoCredito == "FORMACIÓN BÁSICA"){
+                    $creditoFormacionBasica = array(
+                        'Tomados' => $credito->Tomados,
+                        'Flujo' => $credito->Flujo,
+                        'Excedente' => $credito->Excedente,
+                        'IdTipoCredito' => $credito->IdTipoCredito
+                    );
+                }
+                if($credito->TipoCredito == "FORMACIÓN PROFESIONAL"){
+                    $creditoFormacionProfesional = array(
+                        'Tomados' => $credito->Tomados,
+                        'Flujo' => $credito->Flujo,
+                        'Excedente' => $credito->Excedente,
+                        'IdTipoCredito' => $credito->IdTipoCredito
+                    );
+                }
+                if($credito->TipoCredito == "FORMACIÓN HUMANA"){
+                    $creditoHumana = array(
+                        'Tomados' => $credito->Tomados,
+                        'Flujo' => $credito->Flujo,
+                        'Excedente' => $credito->Excedente,
+                        'IdTipoCredito' => $credito->IdTipoCredito
+                    );
+                }
+                if($credito->TipoCredito == "ELECTIVA DE FORMACIÓN HUMANA"){
+                    $creditoElectivaHumana = array(
+                        'Tomados' => $credito->Tomados,
+                        'Flujo' => $credito->Flujo,
+                        'Excedente' => $credito->Excedente,
+                        'IdTipoCredito' => $credito->IdTipoCredito
+                    );
+                }
+            }
+//            $xml = simplexml_load_string($return->CreditosEstudianteResult->Resumen);
+            
+            echo "Creditos Optativa Excendente: ".$creditoOptativo['Excedente'];
+            echo "<br/>";
+            echo "Creditos Libre Opcion Excendente: ".$creditoLibreOpcion['Excedente'];
+            echo "<br/>";
+            echo "Creditos Basico Excendente: ".$creditoFormacionBasica['Excedente'];
+            echo "<br/>";
+            echo "Creditos Profesional Excendente: ".$creditoFormacionProfesional['Excedente'];
+            echo "<br/>";
+            echo "Creditos Humana Excendente: ".$creditoHumana['Excedente'];
+            echo "<br/>";
+            echo "Creditos Electiva Humana Excendente: ".$creditoElectivaHumana['Excedente'];
+//            var_dump($xml);
+            
+        
 //           $aprobadasArray=$xml->NewDataSet->V_MATERIAS_ACREDITADAS;
 //            $numeroAprobadas = $aprobadasArray->count();
 //            echo "Materias Aprobadas: ".$numeroAprobadas;
