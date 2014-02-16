58c6701b76cdcdc7ea78751dde4e871a146d4cd9
Harry Carpio <harry.carpio@gmail.com>
2014-02-15 22:08:05 -0500

    Pagina PHP con la función para determinar el indicador de dificultad.

---
+++ difficultIndicator.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * Descripcion : Determina el indicador del grado de dificultad de una materia.
+ * Intervalo Valores : [0-100]
+ */
+
+function difficultIndicator($num_creditos,$cod_materia){
+    $study_hours;
+    $syllabus;
+    $projects;
+    $class_hours;
+    $teacher_mark;
+    $students_mark;
+    $malla_curricular;
+}
+?>
